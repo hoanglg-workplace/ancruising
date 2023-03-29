bd584be856d0dd8b927c95c99235d29633d9cee0
Lý Gia Hoàng <hoanglg6699@gmail.com>
2023-03-29 11:13:29 +0700

    s

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Body from "./Body/Body";
+import Header from "./Header/Header";
+// import Intro from "./Intro/Intro";
+import Loader from "./Loader/Loader";
+// import MenuLeft from "./Menu_left/Menu_left";
+
+function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
+  return (
+    <div className="App">
+      {/* <Intro /> */}
+      <Header isVisible={isVisible} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Body isVisible={isVisible} setIsVisible={setIsVisible} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Body/Body.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Body.css";
+import MenuLeft from "../Menu_left/Menu_left";
+import SwitchView from "../SwitchView/SwitchView";
+import React360 from "../React360/React360";
+import Intro from "../Intro/Intro";
+
+const Body = (props) => {
+  const [isSwitch, setIsSwitch] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // setTimeout(() => {
+  //   setIsLoading(false);
+  // }, 5400);
+
+  return (
+    <div className="body">
+      <img className="img_logo" src="./hoang_logo.png" alt="" />
+      
+      {isLoading ? (
+        <Intro isPlaying={isLoading} setIsPlaying={setIsLoading} />
+      ) : (
+        <>
+          <React360
+            dir={
+              isSwitch
+                ? "../assets/Images/have_mountain"
+                : "../assets/Images/without_mountain"
+            }
+            numImages={38}
+          />
+          <SwitchView isSwitch={isSwitch} setIsSwitch={setIsSwitch} />
+          <MenuLeft
+            isVisible={props.isVisible}
+            setIsVisible={props.setIsVisible}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Body;
